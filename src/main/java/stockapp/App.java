18270b1faa799d1b52b5bbc62e9bb286c028f761
varshneylavina <lavinavarshney1704@gmail.com>
2024-12-03 10:49:01 +0530

    initial commit

---
+++ src/main/java/stockapp/App.java
@@ -0,0 +1,13 @@
+package stockapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
